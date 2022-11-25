b5f56ee3668a8f49bb810107d582e8257a4273e9
TBtheCoder <boyomichaelbidemi@gmail.com>
2022-11-25 17:04:16 +0100

    ""

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-
 def get(arr):
-    even = 0
-    odd = 0
-    enenNo = None
-    oddNo = None
+    even, odd = 0, 0
+    even_no, odd_no = None, None
     for i in arr:
         if i % 2 == 0:
-            even += 1
-            enenNo = i
+            even += 1;
+            even_no = i
         else:
-            oddNo = i
+            odd_no = i
             odd += 1
-    return enenNo if even == 1 else oddNo
+    return even_no if even == 1 else odd_no
 
 
-
-print(get([2,4,6,7,8,10]))
-
-
+print(get([2, 4, 6, 7, 8, 10]))
